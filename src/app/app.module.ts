ac029c99e1c1c260b68cf016bbcc9c709985f8e1
kutluh <57299132+halilkutlu@users.noreply.github.com>
2020-05-04 09:57:05 +0300

    Assignment 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import {AppRoutingModule} from './app-routing/app-routing.module';
+import {LeaderService} from './services/leader.service';
 
 
 
@@ -43,7 +44,7 @@ import {AppRoutingModule} from './app-routing/app-routing.module';
     MatButtonModule,
     AppRoutingModule
   ],
-  providers: [DishService],
+  providers: [DishService, LeaderService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
