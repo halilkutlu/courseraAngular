689d0666d9ce8165774389b5ea2eebb5f19a7a9f
kutluh <57299132+halilkutlu@users.noreply.github.com>
2020-06-06 20:42:47 +0300

    Directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -34,6 +34,7 @@ import {HttpClientModule} from '@angular/common/http';
 import {baseURL} from './shared/baseurl';
 import {PromotionService} from './services/promotion.service';
 import {ProcessHTTPMsgService} from './services/process-httpmsg.service';
+import { HighlightDirective } from './directives/highlight.directive';
 
 
 
@@ -47,7 +48,8 @@ import {ProcessHTTPMsgService} from './services/process-httpmsg.service';
     AboutComponent,
     HomeComponent,
     ContactComponent,
-    LoginComponent
+    LoginComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
